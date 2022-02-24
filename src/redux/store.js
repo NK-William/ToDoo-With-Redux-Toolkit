cddd4440b824eb70918e9cc7ac04cb811bbf1a99
Nkuna William <NkunaTebogo09@gmail.com>
2022-02-24 09:44:33 +0200

    Added configureStore documentation

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,4 +9,13 @@ const reducer = {
 };
 
 // configureStore simplifies the store setup process
+/***
+ * It combines todosReducer and filtersReducer into the root reducer function, which
+ *  will handle a root state that looks like {todos, filters}, removing the need to
+ *  call combineReducers().
+ * It creates a Redux store using that root reducer, removing the need to call createStore().
+ * It automatically adds the thunk middleware.
+ * It automatically adds more middleware to check for common mistakes like accidentally mutating the state.
+ * It automatically sets up the Redux DevTools Extension connection.
+ */
 export const store = configureStore({ reducer });
